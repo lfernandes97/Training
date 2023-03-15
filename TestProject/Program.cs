13402cc4ef351016baedd7b0bf29959d26d1c0ac
lfernandes97 <leo.fernandes@infotrack.co.uk>
2023-03-15 10:10:48 +0000

    New Repo

---
+++ TestProject/Program.cs
@@ -0,0 +1,22 @@
+﻿
+string[] numbers = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+
+foreach ( string i in numbers)
+{
+    
+    if (i.StartsWith("B"))
+    {  
+        Console.WriteLine(i);
+    }
+    
+;}
+
+string[] orderIDs = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+
+foreach (string orderID in orderIDs)
+{
+    if (orderID.StartsWith("B"))
+    {
+        Console.WriteLine(orderID);
+    }
+}
